778e16b72d7f337ee20886cd02824be74a9cbd21
Ibrahim O Kaware <88645485+iokaware@users.noreply.github.com>
2025-05-29 09:03:12 +0300

    Add files via upload

---
+++ DroidCafe3/app/src/main/java/com/example/droidcafe/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.example.droidcafe;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        OrderActivity orderActivity =  (OrderActivity) getActivity();
+        orderActivity.processDatePickerResult(year, month, day);
+    }
+}
